beccfac4d7eccbe068194d1fbf852e7c7686efeb
atheeqsyed <atheeqmvj@gmail.com>
2022-07-14 21:24:57 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> Hello World</title></head>
+<body>
+
+<form action="process.php" method="Post">
+Enter your name : <Input name ="asdf" type = "text">
+<input type = "submit">
+</form>
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Hello World! </title>
+</head>
+
+<body >
+
+<?php
+    $name = $_POST["asdf"];
+    echo "Hello," . $name;
+?>
+</body>
+</html>
