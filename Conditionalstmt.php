3db8b9f8f929b99c8aa92acff4ca52939a49817e
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-07-23 12:53:10 +0530

    Conditional statements - If, If else,if elseif..,

---
+++ Conditionalstmt.php
@@ -0,0 +1,50 @@
+<?php
+
+echo "<h1>The if Statement</h1>" . "<br>";
+/*if(condition){
+    // Code to be executed
+}*/
+$d = date("D");
+if ($d == "Sat") {
+    echo "Have a nice weekend!";
+}
+echo "<h1>The if...else Statement</h1>" . "<br>";
+/*if(condition){
+    // Code to be executed if condition is true
+} else{
+    // Code to be executed if condition is false
+}*/
+$d = date("D");
+if ($d == "Sat") {
+    echo "Have a nice weekend!";
+} else {
+    echo "Have a nice day!";
+}
+
+
+echo "<h1>The if...elseif...else Statement</h1>" . "<br>";
+/*
+if(condition1){
+    // Code to be executed if condition1 is true
+} elseif(condition2){
+    // Code to be executed if the condition1 is false and condition2 is true
+} else{
+    // Code to be executed if both condition1 and condition2 are false
+}*/
+$d = date("D");
+if ($d == "Sat") {
+    echo "Have a nice weekend!";
+} elseif ($d == "Sun") {
+    echo "Have a nice Sunday!";
+} else {
+    echo "Have a nice day!";
+}
+
+echo "<h1>The Ternary Operator</h1>" . "<br>";
+
+/*if($age < 18){
+    echo 'Child'; // Display Child if age is less than 18
+} else{
+    echo 'Adult'; // Display Adult if age is greater than or equal to 18
+}*/
+echo ($age > 18) ? 'Child' : 'Adult';
