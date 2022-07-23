91c48d2aac24e575a316d2b38ef46f8c27e73ab7
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-07-23 23:48:28 +0530

    Count Words

---
+++ count.php
@@ -0,0 +1,19 @@
+
+<?php
+//count the words in string
+// Input : I LOVE YOU
+//Output : 3
+
+use LDAP\Result;
+
+$var = "I LOVE YOU JAAN you";
+$result = 0;
+for ($i = 0; $i < strlen($var); $i++) {
+    if ($var[$i] === ' ') {
+        $result += 1;
+    }
+}
+$result += 1;
+echo $result;
+
+?>
