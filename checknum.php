6cc43499682e8567849edd869e6f49aca7eed4fc
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-07-23 18:47:37 +0530

    Check the number(+,-,0)

---
+++ checknum.php
@@ -0,0 +1,16 @@
+<html>
+<?php
+//check number is positive, negative
+//$num = $_POST["$check"];
+$num = 12;
+if ($num > 0) {
+    echo "Entered number is Positive : " . ($num);
+} elseif ($num < 0) {
+    echo "Entered number is Negative : " . ($num);
+} else {
+    echo "Entered number is Zero :" . ($num);
+}
+
+?>
+
+</html>
