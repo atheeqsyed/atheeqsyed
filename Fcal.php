fbb49028aceb12b68d8fc22a06389cac0e034da1
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-07-25 07:36:51 +0530

    Add files via upload

--- Fcal.php
+++ Fcal.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <?php
-$first_num = $_POST['first_num'];
-$second_num = $_POST['second_num'];
-$operator = $_POST['operator'];
+$val1 = $_POST['first'];
+$val2 = $_POST['second'];
+$opr_ = $_POST['opr'];
 $result = '';
-switch ($operator) {
-    case "Add":
-        $result = $first_num + $second_num;
+switch ($opr_) {
+    case "+":
+        $result = $val1 + $val2;
         break;
 }
 ?>
@@ -14,15 +14,15 @@ switch ($operator) {
 <body>
     <form action="" method="post" id="quiz-form">
         <p>
-            <b>First Number</b> <input type="number" name="first_num" id="first_num" required="required" value="<?php echo $first_num; ?>" />
+            <b>First Number</b> <input type="number" name="first" id="first" required="required" value="<?php echo $first_num; ?>" />
         </p>
         <p>
-            <b>Second Number</b> <input type="number" name="second_num" id="second_num" required="required" value="<?php echo $second_num; ?>" />
+            <b>Second Number</b> <input type="number" name="second" id="second" required="required" value="<?php echo $second_num; ?>" />
         </p>
         <p>
             <b>Result</b> <input readonly="readonly" name="result" value="<?php echo $result; ?>">
         </p>
-        <input type="submit" name="operator" value="Add" />
+        <input type="submit" name="opr" value="+" />
         <!-- <input type="submit" name="operator" value="Subtract" />
             <input type="submit" name="operator" value="Multiply" />
             <input type="submit" name="operator" value="Divide" /> -->
