adfa56cbe8409ae0459b9daead25a44034d217f6
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-07-25 07:38:32 +0530

    Add files via upload

---
+++ phpbool.php
@@ -0,0 +1,21 @@
+<?php
+ 
+if(TRUE)
+    echo "This condition is TRUE";
+if(FALSE)
+    echo "This condition is not TRUE";
+ 
+ 
+ /*Assign the value TRUE to a variable
+$show_error = True;
+ var_dump($show_error); */
+
+
+/*<?php
+  $submit = true;
+  if ($submit !== false)
+  {
+    echo 'Submitted';
+  }
+?>*/
+?>
