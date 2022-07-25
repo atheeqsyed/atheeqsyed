0021fbd2160ff6eb94915443141d6fbfaf6c580d
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-07-25 07:38:09 +0530

    Add files via upload

---
+++ phparray.php
@@ -0,0 +1,16 @@
+
+<?php
+ 
+$intArray = array( 10, 20 , 30);
+ 
+echo "First Element: $intArray[0]\n";
+echo "<br>";
+echo "Second Element: $intArray[1]\n";
+echo "<br>";
+echo "Third Element: $intArray[2]\n\n";
+echo "<br>";
+ 
+//returns data type and value
+var_dump($intArray);
+ 
+?>
