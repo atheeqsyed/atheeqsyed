064f9879eccc855b2ff566195ba81709fbff2899
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-09-20 21:38:46 +0530

    Update CreateDB.php

--- CreateDB.php
+++ CreateDB.php
@@ -4,7 +4,6 @@ $username = "root";
 $password = "A@#345_abcd1";
 $dbname = "Sample";
 
-
 // Creating a Connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -15,8 +14,6 @@ if ($conn->connect_error){
 /*//Create a Database named Test
 $sql = "CREATE DATABASE Sample";
     */
-
-
 // sql to create table
 /*    $sql = "CREATE TABLE Persons (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
