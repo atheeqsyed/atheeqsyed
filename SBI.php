ee329724ba0a51aa924a49888598dbb67182f95f
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-09-20 21:52:44 +0530

    Update SBI.php

--- SBI.php
+++ SBI.php
@@ -1,15 +1,13 @@
 <?php
-
 class SBI{
     public $amount;
     function __construct($amount){
-        //The $this keyword refers to the current object, and is only available inside methods.
+        //The $this key word refers to the current object, and is only available inside methods.
         $this-> amount= $amount;
     }
 
     public function refund()
     {
-
         return  $this->amount;
     }
 }
