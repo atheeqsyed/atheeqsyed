cc282e3da22748dfd3e71131a0081bea86d576f5
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-09-20 22:03:00 +0530

    Update factnum.php

--- factnum.php
+++ factnum.php
@@ -1,9 +1,8 @@
 <?php
-//factorial of a number (3*2*1)
-//input = 3
-//output = 6
+// Factorial of a number (3*2*1)
+// Input  : 3
+// Output : 6
 $num = 3;
-
 for ($i = 1; $i < 3; $i++) {
     $res =  $num * $i;
 }
