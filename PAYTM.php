398d3334be40a799dfb2d0731c711ff2ad3d7452
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-09-20 21:50:55 +0530

    Update PAYTM.php

--- PAYTM.php
+++ PAYTM.php
@@ -1,12 +1,11 @@
 <?php
-
 class PAYTM extends SBI {
     public $wallet;
     public function __construct($wallet)
     {
         $this->wallet= $wallet;
     }
-//The purpose of overriding is to change the behavior of parent class method.
+//*The purpose of overriding is to change the behavior of parent class method*.
     public function refund($account){
         return $this->wallet;
     }
