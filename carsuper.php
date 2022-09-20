554e13a21f507cbd8e36e170ed7d9c2dbed18d71
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-09-20 21:56:56 +0530

    Update carsuper.php

--- carsuper.php
+++ carsuper.php
@@ -2,10 +2,8 @@
 
 include ('Car2.php');
 include ('SuperCar.php');
-
 //$rprice = new Car("Santro", "23 - 12 - 2022", 1000);
 $sprice = new SuperCar("Swift", "10 - 10 - 2022", 1500);
 //echo $rprice->purchase() . "\n";
 //$rprice->purchase();
 $sprice->purchase();
-
