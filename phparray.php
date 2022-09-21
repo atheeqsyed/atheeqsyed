6dd6734ac6ed6c35b32203645a82267134cafe96
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-09-21 07:14:25 +0530

    Update phparray.php

--- phparray.php
+++ phparray.php
@@ -1,13 +1,15 @@
 
 <?php
  
-$intArray = array( 10, 20 , 30);
+$intArray = array( 10, 20 , 30,40);
  
 echo "First Element: $intArray[0]\n";
 echo "<br>";
 echo "Second Element: $intArray[1]\n";
 echo "<br>";
 echo "Third Element: $intArray[2]\n\n";
+echo "<br>";
+echo "Fourth Element: $intArray[3]\n\n";
 echo "<br>";
  
 //returns data type and value
