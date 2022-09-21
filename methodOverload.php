18658d50d8a43c10226fa84e5fef3a0da24d315d
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-09-21 07:12:06 +0530

    Update methodOverload.php

--- methodOverload.php
+++ methodOverload.php
@@ -2,18 +2,13 @@
 //Method overloading - Where two or more methods have same method name with different arguments or parameters and return type.
 // Constructor, Operator and Method Overloading
 
-
-//Method Overloading
+//Method Overloading concept
 // utilize php's magic method __call()
 class Cal{
-
     function add($a, $b,$c){
         return $this->$a+$b+$c;
     }
-
-
 }
 
 $adding = new methodOverloading(2,3);
 echo $adding->add();
-
