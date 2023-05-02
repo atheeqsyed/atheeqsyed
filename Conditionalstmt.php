9ce89443e0c352ea15fb7d40c4f6c511d3714514
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2023-05-02 10:09:31 +0530

    Update Conditionalstmt.php

--- Conditionalstmt.php
+++ Conditionalstmt.php
@@ -1,21 +1,21 @@
 <?php
 echo "<h1>The if Statement</h1>" . "<br>";
 /*if(cond){
-    // Code to be executed
+    //Code to be executed
 }*/
 $d = date("D");
 if ($d == "Sat") {
-    echo "Have a nice weekend!";
+    echo "Have a nice weekend!.";
 }
 echo "<h1>The if...else Statement</h1>" . "<br>";
 /*if(cond){
-    // Code to be executed if condition is true
+    //Code to be executed if condition is true
 } else{
     // Code to be executed if condition is false
 }*/
 $d = date("D");
 if ($d == "Sat") {
-    echo "Have a nice weekend!";
+    echo "Have a nice weekend!.";
 } else {
     echo "Have a nice day!";
 }
@@ -24,19 +24,19 @@ if ($d == "Sat") {
 echo "<h1>The if...elseif...else Statement</h1>" . "<br>";
 /*
 if(cond1){
-    // Code to be executed if condition1 is true
+    //Code to be executed if condition1 is true
 } elseif(cond2){
-    // Code to be executed if the condition1 is false and condition2 is true
+    //Code to be executed if the condition1 is false and condition2 is true
 } else{
-    // Code to be executed if both condition1 and condition2 are false
+    //Code to be executed if both condition1 and condition2 are false
 }*/
 $d = date("D");
 if ($d == "Sat") {
-    echo "Have a nice weekend!";
+    echo "Have a nice weekend!.";
 } elseif ($d == "Sun") {
-    echo "Have a nice Sunday!";
+    echo "Have a nice Sunday!.";
 } else {
-    echo "Have a nice day!";
+    echo "Have a nice day!.";
 }
 
 echo "<h1>The Ternary Operator</h1>" . "<br>";
